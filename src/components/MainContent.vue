34b6a15aafea622e64263b10244be454596880c0
BAKRI Anouar <bakri.anouar@gmail.com>
2022-04-27 16:21:13 +0200

    feat: add working tabs

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -19,84 +19,13 @@ export default {
     },
   },
 };
-
-function formatXml(xml, colorize) {
-  function esc(s) {
-    return s.replace(/[-\/&<> ]/g, function (c) {
-      // Escape special chars
-      return c == " " ? "&nbsp;" : "&#" + c.charCodeAt(0) + ";";
-    });
-  }
-  var sm = '<div class="xmt">',
-    se = '<div class="xel">',
-    sd = '<div class="xdt">',
-    sa = '<div class="xat">',
-    tb = '<div class="xtb">',
-    tc = '<div class="xtc">',
-    ind = "  ",
-    sz = "</div>",
-    tz = "</div>",
-    re = "",
-    is = "",
-    ib,
-    ob,
-    at,
-    i;
-  if (!colorize) sm = se = sd = sa = sz = "";
-  xml
-    .match(/(?<=<).*(?=>)|$/s)[0]
-    .split(/>\s*</)
-    .forEach(function (nd) {
-      ob = ("<" + nd + ">").match(/^(<[!?\/]?)(.*?)([?\/]?>)$/s); // Split outer brackets
-      ib = ob[2].match(/^(.*?)>(.*)<\/(.*)$/s) || ["", ob[2], ""]; // Split inner brackets
-      at = ib[1].match(/^--.*--$|=|('|").*?\1|[^\t\n\f \/>"'=]+/g) || [""]; // Split attributes
-      if (ob[1] == "</") is = is.substring(ind.length); // Decrease indent
-      re +=
-        tb +
-        tc +
-        esc(is) +
-        tz +
-        tc +
-        sm +
-        esc(ob[1]) +
-        sz +
-        se +
-        esc(at[0]) +
-        sz;
-      for (i = 1; i < at.length; i++)
-        re +=
-          (at[i] == "="
-            ? sm + "=" + sz + sd + esc(at[++i])
-            : sa + " " + at[i]) + sz;
-      re += ib[2]
-        ? sm +
-          esc(">") +
-          sz +
-          sd +
-          esc(ib[2]) +
-          sz +
-          sm +
-          esc("</") +
-          sz +
-          se +
-          ib[3] +
-          sz
-        : "";
-      re += sm + esc(ob[3]) + sz + tz + tz;
-      if (ob[1] + ob[3] + ib[2] == "<>") is += ind; // Increase indent
-    });
-  return re;
-}
 </script>
-
 <template>
-  <div class="bg-light pt-5 ps-5 pe-5 rounded workarea">
-    <prism-editor
-      class="my-editor"
-      v-model="data"
-      :highlight="highlighter"
-      line-numbers
-    ></prism-editor>
+  <div class="tab-content workarea" id="pills-tabContent">
+    <div class="tab-pane fade show active codearea pt-5 ps-5 pe-5" id="pills-code" role="tabpanel" aria-labelledby="pills-code-tab">
+      <prism-editor class="my-editor" v-model="data" :highlight="highlighter" line-numbers></prism-editor>
+    </div>
+    <div class="tab-pane fade codearea pt-5 ps-5 pe-5" id="pills-design" role="tabpanel" aria-labelledby="pills-design-tab">design</div>
   </div>
 </template>
 
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,39 +1,43 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="d-flex flex-column pt-5 flex-shrink-0 bg-light sidebar">
-    <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-      <li class="nav-item">
+  <div class="d-flex flex-column pt-5 flex-shrink-0 sidebar">
+    <ul class="nav nav-pills nav-flush flex-column mb-auto text-center" id="pills-tab" role="tablist">
+      <li class="nav-item" role="presentation">
         <a
-          href="#"
           class="nav-link active py-3 border-bottom"
-          aria-current="page"
+          id="pills-code-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#pills-code"
           title="Code"
-          data-bs-toggle="tooltip"
-          data-bs-placement="right"
+          role="tab"
+          aria-controls="pills-code"
+          aria-current="page"
+          aria-selected="true"
+          href="#"
         >
           <svg class="bi" width="24" height="24" role="img" aria-label="Code">
             <use xlink:href="#code" />
           </svg>
+          Code
         </a>
       </li>
-      <li>
+      <li class="nav-item" role="presentation">
         <a
-          href="#"
           class="nav-link py-3 border-bottom"
+          id="pills-design-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#pills-design"
           title="Diagram"
-          data-bs-toggle="tooltip"
-          data-bs-placement="right"
+          role="tab"
+          aria-controls="pills-design"
+          aria-selected="false"
+          href="#"
         >
-          <svg
-            class="bi"
-            width="24"
-            height="24"
-            role="img"
-            aria-label="Diagram"
-          >
+          <svg class="bi" width="24" height="24" role="img" aria-label="Diagram">
             <use xlink:href="#diagram" />
           </svg>
+          Design
         </a>
       </li>
     </ul>
@@ -42,7 +46,8 @@
 
 <style scoped>
 .sidebar {
-  width: 4.5rem;
+  width: 5rem;
+  background-color: #f3f4f5;
 }
 
 .bi {
