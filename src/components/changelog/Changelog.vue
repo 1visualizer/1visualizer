a70bb0531acb1a7bcb81d227a2b1317c6b24a990
BAKRI Anouar <bakri.anouar@gmail.com>
2022-06-19 18:48:02 +0200

    feat: add changelog page

---
+++ src/components/changelog/Changelog.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import Markdown from "vue3-markdown-it";
+
+const source = ref("");
+onMounted(function () {
+  fetch("../../../CHANGELOG.md")
+    .then((response) => response.text())
+    .then((text) => {
+      source.value = text;
+    });
+});
+</script>
+
+<template>
+  <main id="changelog" class="fluid-container">
+    <Markdown :source="source" />
+  </main>
+</template>
+
+<style scoped>
+.fluid-container {
+  height: 93vh;
+}
+</style>
